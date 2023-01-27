0a5caf5779b81103495c5fd53679083b833d762e
Dmytro Hirich <diman12345677@gmail.com>
2023-01-27 12:41:45 +0200

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-[#3e3e3e] h-[100vh] w-full flex flex-col">
+      HELLO
+    </div>
+  );
+};
+
+export default Home;
